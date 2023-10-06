c45fbe4eed88602665e45eefc59b36bb1e923277
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2023-10-05 23:44:39 -0300

    teste
    teste

--- frontend/src/components/FormCliente/index.jsx
+++ frontend/src/components/FormCliente/index.jsx
@@ -1,37 +1,74 @@
 import { Caixa, CaixaForm } from "./styles";
+import { useState } from "react";
+import axios from 'axios';
 
 function FormCliente(){
+
+    const [usuario, setUsuario] = useState({
+        nome: '',
+        telefone: '',
+        dataDeNascimento: '',
+        cpf: '',
+        email: '',
+
+    });
+
+    const handleChange = (event) =>{
+        const {name, value} = event.target;
+        setUsuario({...usuario,
+            [name]:value,
+        });
+    };
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        try{
+            const response = await axios.post('http://localhost:8080/clientes', usuario, {
+  
+    })
+
+
+    }
+    catch(error){
+        console.error('Erro ao Cadastrar', error)
+    }
+
+    console.log(usuario.nome)
+}
+    
     return(
         <Caixa>
         <CaixaForm >
             <h1>CLiente</h1>
-            <form >
+            <form onSubmit={handleSubmit}>
                 <label For="nome">Nome:</label>
-                <input type="text" name="nome" id=""/><br/>
+                <input type="text" name="nome" id="" onChange={handleChange} value={usuario.nome} /><br/>
                 
                 <label For="nome">Telefone:</label>
-                <input type="text" name="" id=""/><br/>
+                <input type="text" name="" id="" onChange={handleChange} value={usuario.telefone}/><br/>
 
                 <label For="nome">Data Nascimento:</label>
-                <input type="text" name="" id=""/><br/>
+                <input type="text" name="" id="" onChange={handleChange} value={usuario.dataDeNascimento}/><br/>
 
                 <label For="nome">CPF:</label>
-                <input type="text" name="" id=""/><br/>
+                <input type="text" name="" id="" onChange={handleChange} value={usuario.cpf}/><br/>
 
                 <label For="nome">E-mail:</label>
-                <input type="text" name="" id=""/><br/>
+                <input type="text" name="" id="" onChange={handleChange} value={usuario.email}/><br/>
 
                 <label For="nome">CEP:</label>
-                <input type="text" name="" id=""/><br/>
+                <input type="text" name="" id="" onChange={handleChange}/><br/>
 
                 <label For="nome">Rua:</label>
-                <input type="text" name="" id=""/><br/>
+                <input type="text" name="" id="" onChange={handleChange}/><br/>
 
                 <label For="nome">Bairro:</label>
-                <input type="text" name="" id=""/><br/>
+                <input type="text" name="" id="" onChange={handleChange}/><br/>
 
                 <label For="nome">Cidade:</label>
-                <input type="text" name="" id=""/><br/>
+                <input type="text" name="" id="" onChange={handleChange} /><br/>
+
+                <button type="submit">Cadastrar</button>
 
             </form>
         </CaixaForm >
@@ -61,6 +98,8 @@ function FormCliente(){
 
                 <label for="relato">Problema Relatado:</label>
                 <textarea id="relat" name="relato" rows="4" cols="50"></textarea>
+
+                <button>Cadastrar</button>
 
 
                
